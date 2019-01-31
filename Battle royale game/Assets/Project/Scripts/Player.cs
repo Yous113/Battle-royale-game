59cf9a805498fce4affd77bbbe47e2c2a692dafb
Yous113 <44925411+Yous113@users.noreply.github.com>
2019-01-31 19:11:35 +0100

    31-01-2019

--- Battle royale game/Assets/Project/Scripts/Player.cs
+++ Battle royale game/Assets/Project/Scripts/Player.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public string playerName = "Yous";
+    public int age = 15;
+    private float health = 100.0f;
+    
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello World!");
+        Debug.Log("Hello" + playerName);
+        Debug.Log("You were born at" + (2018 - age));
+        Debug.Log("Health:" + health);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Update!");
+     
     }
 }

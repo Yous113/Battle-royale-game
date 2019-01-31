22bb36aff98d335e56460f1e74411b9b19b85070
Yous113 <44925411+Yous113@users.noreply.github.com>
2019-01-31 18:47:02 +0100

    Update Player.cs

--- Battle royale game/Assets/Project/Scripts/Player.cs
+++ Battle royale game/Assets/Project/Scripts/Player.cs
@@ -7,12 +7,12 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello World");
+        Debug.Log("Hello World!");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("Update!");
     }
 }

ffcaf43d13842801e5fa6e05318b0a938a85ec4b
Yous113 <44925411+Yous113@users.noreply.github.com>
2019-02-04 18:31:42 +0100

    gamecamera

---
+++ Battle royale game/Assets/Project/Scripts/GameCamera.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCamera : MonoBehaviour
+{
+    [SerializeField] private GameObject target;
+    [SerializeField] private Vector3 translationOffset;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+    }
+
+    private void FixedUpdate()
+    {
+        transform.LookAt(target.transform.position + translationOffset);
+    }
+}

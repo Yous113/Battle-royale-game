36f81506d21d842f0695955d3dfebcca9813ec0d
Yous113 <44925411+Yous113@users.noreply.github.com>
2019-02-12 16:23:03 +0100

    Vertical look
    next thing Rotation point

--- Battle royale game/Assets/Project/Scripts/GameCamera.cs
+++ Battle royale game/Assets/Project/Scripts/GameCamera.cs
@@ -7,6 +7,8 @@ public class GameCamera : MonoBehaviour
     [SerializeField] private GameObject target;
     [SerializeField] private Vector3 translationOffset;
     [SerializeField] private Vector3 followOffset;
+
+    private float verticalRotationAngle;
     
     // Start is called before the first frame update
     void Start()
@@ -21,11 +23,16 @@ public class GameCamera : MonoBehaviour
     }
 
     private void FixedUpdate()
-    {      
+    {
+        // Make the camera look at the target.
         float yAngle = target.transform.eulerAngles.y;
         Quaternion rotation = Quaternion.Euler(0, yAngle, 0);
 
         transform.position = target.transform.position - (rotation * followOffset);
         transform.LookAt(target.transform.position + translationOffset);
+
+        // Make the camera look up or down.
+        verticalRotationAngle += Input.GetAxis("Mouse Y");
+        transform.RotateAround(target.transform.position, target.transform.right, -verticalRotationAngle);
     }
 }

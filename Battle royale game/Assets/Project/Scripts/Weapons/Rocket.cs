efc9664f30330526a1107c93b180078f5240041a
Yous113 <44925411+Yous113@users.noreply.github.com>
2019-07-18 13:44:27 +0200

    video 96

--- Battle royale game/Assets/Project/Scripts/Weapons/Rocket.cs
+++ Battle royale game/Assets/Project/Scripts/Weapons/Rocket.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    [Header("Rocket")]
     [SerializeField] private float speed;
+    [SerializeField] private float lifetime;
+
+    [Header("Explosion")]
+    [SerializeField] private GameObject explosionPrefab;
+    [SerializeField] private float explosionRange;
+    [SerializeField] private float explosionDamage;
 
     private Rigidbody rocketRigidbody;
+    private float timer;
     
     // Start is called before the first frame update
     void Awake()
@@ -17,12 +25,29 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+        if(timer >= lifetime)
+        {
+            Explode();
+        }
     }
 
     public void Shoot (Vector3 direction)
     {
         transform.forward = direction;
         rocketRigidbody.velocity = direction * speed;
+    }
+
+    public void OnTriggerEnter(Collider otherCollider)
+    {
+        Explode();
+    }
+
+    private void Explode()
+    {
+        GameObject explosion = Instantiate(explosionPrefab);
+        explosion.transform.position = transform.position;
+        explosion.GetComponent<Explosion>().Explode(explosionRange, explosionDamage);
+        Destroy(gameObject);
     }
 }

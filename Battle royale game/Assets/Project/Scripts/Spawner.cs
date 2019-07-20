e69d7cb42eb12feef6ad91714232a334a9af01e2
Yous113 <44925411+Yous113@users.noreply.github.com>
2019-07-20 02:23:12 +0200

    video 116

---
+++ Battle royale game/Assets/Project/Scripts/Spawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Spawner : NetworkBehaviour
+{
+    [SerializeField] private GameObject prefab;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject instance = Instantiate(prefab, transform.position, transform.rotation, transform.parent);
+
+        NetworkServer.Spawn(instance);
+
+        Destroy(gameObject);
+    }
+}
